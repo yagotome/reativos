5bbbdef14a81509fc920e9da2f5dd784eadacec8
Lucas Alves <lucas_comp@outlook.com>
2017-05-17 10:38:09 -0300

    [IGNORAR] Projeto em desenvolvimento

--- mini-arduino/leitorRFID.ino
+++ mini-arduino/leitorRFID.ino
@@ -1,66 +1,56 @@
 /*
-	Programa: Controle de Acesso com leitor RFID
-	Autores: Lucas Alves
-		 Gustavo Silva
-		 Luiz Andrade
+  Programa: Controle de Acesso com leitor RFID
+  Autores: Lucas Alves
+           Gustavo Silva
+           Luiz Andrade
 */
 
 #include <SPI.h>
 #include <MFRC522.h>
 #include <LiquidCrystal.h>
+#include <Servo.h>
 
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Cria instância MFRC522
 
 LiquidCrystal lcd(6, 7, 5, 4, 3, 2); // Inicializa a biblioteca com os números dos pins da interface
+Servo servo;
 
 void setup() {
+	Serial.begin(9600);   // Inicia a serial
+	SPI.begin();      // Inicia  SPI bus
+	mfrc522.PCD_Init();   // Inicia MFRC522
 	lcd.begin(16, 2); //Define o número de colunas e linhas do LCD
-	msgInicial();
+	servo.attach(1);
+	servo.write(0);
 }
 
 void loop() {
-	if (! mfrc522.PICC_IsNewCardPresent()) { // Procura por novos cartões
-		return;
+	msgInicial();
+  
+	if (!mfrc522.PICC_IsNewCardPresent()) { // Procura por novos cartões
+  		return;
 	}
   
-	if (! mfrc522.PICC_ReadCardSerial()) { // Aceita somente se um cartão for lido por vez
-		return;
-	}
+	if (!mfrc522.PICC_ReadCardSerial()) { // Aceita somente se um cartão for lido por vez
+    		return;
+  	}
   
-	Serial.print("UID da tag :"); // Exibe UID na serial
-	String conteudo= "";
-	byte letra;
-	
-	for (byte i = 0; i < mfrc522.uid.size; i++) { // Testar o que acontece na prática
-		Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-		Serial.print(mfrc522.uid.uidByte[i], HEX);
-		conteudo.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-		conteudo.concat(String(mfrc522.uid.uidByte[i], HEX));
-	}
-	
-	conteudo.toUpperCase();
-	  
-	if (conteudo.substring(1) == "ED 78 03 CA") { //UID 1 - Chaveiro (verificar parâmetro)
-		lcd.clear();
-		lcd.setCursor(0,0);
-		lcd.print("Ola, professor!");
-		lcd.setCursor(0,1);
-		lcd.print("Acesso liberado!");
-		delay(3000);
-		mensageminicial();
-	}
- 
-	if (conteudo.substring(1) == "BD 9B 06 7D") { //UID 2 - Cartao (verificar parâmetro)
-		lcd.clear();
-		lcd.setCursor(0,0);
-		lcd.print("Ola, aluno!");
-		lcd.setCursor(0,1);
-		lcd.print("Acesso Negado!");
-		delay(3000);
-		mensageminicial();
-	}
+  	Serial.print("UID da tag :"); // Exibe UID na serial
+  	String conteudo= "";
+  
+  	for (byte i = 0; i < mfrc522.uid.size; i++) {
+    		Serial.print(mfrc522.uid.uidByte[i]);
+    		conteudo.concat(String(mfrc522.uid.uidByte[i]));
+  	}
+  
+  	Serial.println();
+
+	if(validar(conteudo)) {
+    		rotacionarServo();
+  	}
+	delay(3000);
 }
 
 void msgInicial() {
@@ -68,4 +58,43 @@ void msgInicial() {
 	lcd.print(" Aproxime o seu");  
 	lcd.setCursor(0,1);
 	lcd.print("cartao do leitor");
-}	
+}
+
+bool validar(String conteudo)  {
+  	lcd.clear();
+  	lcd.setCursor(0,0);
+  
+  	if(conteudo == "2297012136") { //UID 1 - Chaveiro
+    		lcd.print("Ola, professor!");
+    		lcd.setCursor(0,1);
+    		lcd.print("Acesso liberado!");
+    		return true;
+ 	}
+	if(conteudo == "174184252154") { //UID 2 - RioCard do Lucas
+	   	lcd.print("Ola, Lucas!");
+	    	lcd.setCursor(0,1);
+	    	lcd.print("Seja bem-vindo!");
+	    	return true;
+	}
+	if(conteudo == "23518863174") { //UID 3 - BU do Luiz
+	    	lcd.print("Ola, Luiz!");
+	    	lcd.setCursor(0,1);
+	    	lcd.print("Seja bem-vindo!");
+	    	return true;
+	}
+	if(conteudo == "11111186222") { //UID 4 - RioCard do Gustavo
+		lcd.print("Ola, Gustavo!");
+		lcd.setCursor(0,1);
+		lcd.print("Seja bem-vindo!");
+		return true;
+	}
+	//Cartões desconhecidos
+	lcd.print("Ola, aluno!");
+	lcd.setCursor(0,1);
+	lcd.print("Acesso Negado!");
+	return false;
+}
+
+void rotacionarServo() {
+	servo.write(180);
+}
