1396adef3d945c094233f0609ff25c1261fd2659
Lucas Alves <lucas_comp@outlook.com>
2017-05-30 18:55:48 -0300

    Exercício - "Animação"

--- tarefas/twoSquares.c
+++ tarefas/twoSquares.c
@@ -1,11 +1,9 @@
-// Falta testar
-
 #include <SDL2/SDL.h>
 #include <assert.h>
 
 int main (int argc, char* args[]) {
 
-        /* INITIALIZATION */
+	/* INITIALIZATION */
 
 	int err = SDL_Init(SDL_INIT_EVERYTHING);
 	assert(err == 0);
@@ -21,15 +19,19 @@ int main (int argc, char* args[]) {
 	SDL_Rect r1 = {10, 90, 50, 50}, r2 = {330, 90, 50, 50};
 	SDL_Event e;
 	
-	int speed1 = 15, speed2 = 10;
-	
+	float speed1 = 0.04, speed2 = 0.03, s1, s2;
+	int dir1=0, dir2=0;
+
 	while(1) {
-	
+
 		SDL_PollEvent(&e);
 		
 		/* Eventos */
 		if(e.type == SDL_QUIT) {
-			break;
+			/* FINALIZATION */
+			SDL_DestroyWindow(window);
+			SDL_Quit();
+			return 0;
 		}
 		if(e.type == SDL_MOUSEBUTTONDOWN) {
 			SDL_MouseButtonEvent* me = (SDL_MouseButtonEvent*) &e;
@@ -39,35 +41,95 @@ int main (int argc, char* args[]) {
 			else if((me->x >= r2.x) && (me->x <= r2.x + 50) && (me->y >= r2.y) && (me->y <= r2.y + 50)) {
 				speed2 = 0;
 			}
+			if(speed1 == 0 && speed2 == 0) {
+				while(1) {
+					SDL_WaitEvent(&e);
+					if(e.type == SDL_QUIT) {
+						/* FINALIZATION */
+						SDL_DestroyWindow(window);
+						SDL_Quit();
+						return 0;
+					}
+				}
+			}
 		}
-		
+
+		s1 += speed1;
+		s2 += speed2;
+
 		/* Animações */
-		if((r1.x >= 10 && r1.x < 260) && (r1.y == 90 || r1.y == 340)) r1.x += speed1;
-		else r1.y += speed1;
-		if((r1.x == 10 && r1.y == 90) || (r1.x == 260 && r1.y == 340)) speed1 *= -1;
-		
-		if((r2.x >= 330 && r2.x < 580) && (r2.y == 90 || r2.y == 340)) r2.x += speed2;
-		else r2.y += speed2;
-		if((r2.x == 330 && r2.y == 90) || (r2.x == 580 && r2.y == 340)) speed2 *= -1;
+		if(dir1 == 0) {
+			r1.x += (int)s1;
+			if(r1.x > 260) {
+				r1.x = 260;
+				dir1 = 1;
+			}
+		}
+		else if(dir1 == 1) {
+			r1.y += (int)s1;
+			if(r1.y > 340) {
+				r1.y = 340;
+				dir1 = 2;
+			}
+		}
+		else if(dir1 == 2) {
+			r1.x -= (int)s1;
+			if(r1.x < 10) {
+				r1.x = 10;
+				dir1 = 3;
+			}
+		}
+		else {
+			r1.y -= (int)s1;
+			if(r1.y < 90) {
+				r1.y = 90;
+				dir1 = 0;
+			}
+		}
+
+		if(dir2 == 0) {
+			r2.x += (int)s2;
+			if(r2.x > 580) {
+				r2.x = 580;
+				dir2 = 1;
+			}
+		}
+		else if(dir2 == 1) {
+			r2.y += (int)s2;
+			if(r2.y > 340) {
+				r2.y = 340;
+				dir2 = 2;
+			}
+		}
+		else if(dir2 == 2) {
+			r2.x -= (int)s2;
+			if(r2.x < 330) {
+				r2.x = 330;
+				dir2 = 3;
+			}
+		}
+		else {
+			r2.y -= (int)s2;
+			if(r2.y < 90) {
+				r2.y = 90;
+				dir2 = 0;
+			}
+		}
+
+		if(s1 >= 1) s1 = 0;
+		if(s2 >= 1) s2 = 0;
 		
 		/* Redesenho */
 		SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0x00);
-                SDL_RenderFillRect(renderer, NULL);
+		SDL_RenderFillRect(renderer, NULL);
 		
 		SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0x00);
-                SDL_RenderFillRect(renderer, &r1);
+		SDL_RenderFillRect(renderer, &r1);
 		
-		SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0xFF, 0x00);
-                SDL_RenderFillRect(renderer, &r2);
+		SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0x00);
+		SDL_RenderFillRect(renderer, &r2);
         
-                SDL_RenderPresent(renderer);
+		SDL_RenderPresent(renderer);
 	
 	}
-	
-	/* FINALIZATION */
-
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-
-	return 0;
 }
